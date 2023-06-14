d9522037008416c1d15bc0d0b736a37b734ae0d0
ahmet <ahmet.altun60@gmail.com>
2023-06-14 20:54:23 +0300

    Init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {browser: true, es2020: true},
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+        'prettier'
+    ],
+    parserOptions: {
+        "ecmaVersion": 2021,
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    settings: {react: {version: '18.2'}},
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': 'warn',
+        'react/prop-types': 'off',
+    },
+}
